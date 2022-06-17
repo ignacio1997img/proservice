026e607f81f237cd518708e0831ac89d36d9f3d3
ignacio1997img+ <ignaciomolinaguzman20@gmail.com>
2022-06-17 14:29:37 -0400

    teams

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -371,7 +371,7 @@
               </div>
             </div>
             <div class="member-info">
-              <h4>Walter White</h4>
+              {{-- <h4>Walter White</h4> --}}
               <span>Seguridad</span>
             </div>
           </div>
@@ -389,7 +389,7 @@
               </div>
             </div>
             <div class="member-info">
-              <h4>Sarah Jhonson</h4>
+              {{-- <h4>Sarah Jhonson</h4> --}}
               <span>Jardineria</span>
             </div>
           </div>
@@ -407,7 +407,7 @@
               </div>
             </div>
             <div class="member-info">
-              <h4>William Anderson</h4>
+              {{-- <h4>William Anderson</h4> --}}
               <span>Limpieza de Piscinas</span>
             </div>
           </div>
@@ -425,7 +425,7 @@
               </div>
             </div>
             <div class="member-info">
-              <h4>Amanda Jepson</h4>
+              {{-- <h4>Amanda Jepson</h4> --}}
               <span>Modelaje</span>
             </div>
           </div>
