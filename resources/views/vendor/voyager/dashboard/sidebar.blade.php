e85ee1fea8627d121c9d4c4cd824faa277718f19
ignacio1997img+ <ignaciomolinaguzman20@gmail.com>
2022-06-30 11:37:08 -0400

    icono

--- resources/views/vendor/voyager/dashboard/sidebar.blade.php
+++ resources/views/vendor/voyager/dashboard/sidebar.blade.php
@@ -16,7 +16,7 @@
             </div> --}}
 
             <div class="panel widget center bgimage"
-                 style="background-image:url({{ Voyager::image( Voyager::setting('admin.bg_image'), asset('images/banner.jpg') ) }}); background-size: cover; background-position: 0px;">
+                 style="background-image:url({{ Voyager::image( Voyager::setting('admin.bg_image'), asset('images/banner.jpeg') ) }}); background-size: cover; background-position: 0px;">
                 <div class="dimmer"></div>
                 <div class="panel-content">
                     <img src="{{ $user_avatar }}" class="avatar" alt="{{ Auth::user()->name }} avatar">
