7c857463f2af4fdb6a4ea0cd42ba16c2dee067a0
ignacio1997img+ <ignaciomolinaguzman20@gmail.com>
2022-06-30 11:53:29 -0400

    si

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,8 +10,13 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="template/assets/img/icono.png" rel="icon">
-  <link href="template/assets/img/icono.png" rel="apple-touch-icon">
+  {{-- <link rel="shortcut icon" href="{{ asset('images/icon.png') }}" type="image/png"> --}}
+
+  <link href="{{ asset('images/icon.png') }}" rel="icon">
+  <link href="{{ asset('images/icon.png') }}" rel="apple-touch-icon">
+
+  {{-- <link href="template/assets/img/icono.png" rel="icon">
+  <link href="template/assets/img/apple-touch-icon.png" rel="apple-touch-icon"> --}}
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
